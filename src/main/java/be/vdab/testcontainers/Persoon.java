7dc0ec50d3064146d25e38d213de6da8b74e0e16
Wietse Mees <wietsemees@hotmail.com>
2024-01-12 14:38:35 +0100

    Initial commit

---
+++ src/main/java/be/vdab/testcontainers/Persoon.java
@@ -0,0 +1,18 @@
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "personen")
+class Persoon {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private long id;
+    private String voornaam;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getVoornaam() {
+        return voornaam;
+    }
+}
+
