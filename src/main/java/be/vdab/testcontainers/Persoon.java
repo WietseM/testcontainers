71238f449c16839eb1dcb6a02e846fa4d0af0327
Wietse Mees <wietsemees@hotmail.com>
2024-01-12 14:38:58 +0100

    Testcontainer

--- src/main/java/be/vdab/testcontainers/Persoon.java
+++ src/main/java/be/vdab/testcontainers/Persoon.java
@@ -1,10 +1,13 @@
+package be.vdab.testcontainers;
+
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "personen")
 class Persoon {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) private long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String voornaam;
 
     public long getId() {
--- src/test/java/be/vdab/testcontainers/PersoonRepositoryTest.java
+++ src/test/java/be/vdab/testcontainers/PersoonRepositoryTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package be.vdab.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJpaTest
+@Sql("/insertPersonen.sql")
 class PersoonRepositoryTest {
-  
+    private final PersoonRepository repository;
+    @Container
+    @ServiceConnection
+    private static final MySQLContainer mySQL =
+            new MySQLContainer("mysql:latest")
+            .withDatabaseName("testcontainers")
+            .withUsername("cursist")
+            .withPassword("cursist");
+
+    public PersoonRepositoryTest(PersoonRepository repository) {
+        this.repository = repository;
+    }
+
+    @Test
+    void erIs1PersoonEnDatIsJoe() {
+        assertThat(repository.findAll())
+                .singleElement()
+                .extracting(Persoon::getVoornaam)
+                .isEqualTo("Joe");
+    }
 }
